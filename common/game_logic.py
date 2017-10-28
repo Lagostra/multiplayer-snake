fe9222628a887e40253939c8032a61098d654b59
Dodib <duong_mnb@hotmail.com>
2017-10-28 22:20:37 +0200

    test moving snake

--- common/game_logic.py
+++ common/game_logic.py
@@ -4,30 +4,27 @@ from level import Level
 class GameLogic:
 
     def __init__(self):
-        self.level = Level(30, 20)
+        self.level = Level((30, 20))
+        self.dir_dict = {0: (0, -1), 1: {1, 0}, 2: {0, 1}, 3: {-1, 0}}
 
     def player_move(self, snake, direction):
+        # check if valid - fix this one
         filter(lambda x: x == snake, self.level.snake_list).direction = direction
 
     def tick(self):
-        for snakes in self.level.snakes_list:
-            self.move_snake(snakes)
-        self.check_collision()
+        temp_level = self.level
+        for snake in temp_level.snakes:
+            self.move_snake(snake)
+        self.level = temp_level
 
     def move_snake(self, snake):
-        dir_mod = {0: (0, -1), 1: {1, 0}, 2: {0, 1}, 3: {-1, 0}}
-        new_head = (snake.body[0][0] + dir_mod[snake.direction][0], snake.body[0][1] + dir_mod[snake.direction][0][1])
-        if new_head not in self.level.apples_list:
-            snake.body.pop()
-        else:
-            self.level.apples_list.pop(self.level.apples_list.find(new_head))
-        snake.insert(0, new_head)
+        snake.body.insert(snake.body[0][0] + self.dir_dict[snake.direction][0],
+                          snake.body[0][1] + self.dir_dict[snake.direction][1])
 
-    def check_collision(self):
-        for snakes in self.level.snakes_list:
-            for other_snake in filter(lambda x: x != snakes, self.level.snakes_list):
-                if snakes.body[0] in other_snake.body:
-                    pass
+
+
+
+
 
 
 
