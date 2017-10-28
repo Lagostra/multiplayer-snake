45bde3368cb6e7f50bf1b04826f72e595dbb8d0c
Eivind Lie Andreassen <ela4991@gmail.com>
2017-10-28 23:43:44 +0200

    Added snakes and a block to level for debugging

--- common/level.py
+++ common/level.py
@@ -1,3 +1,5 @@
+from block import Block
+from snake import Snake
 
 
 class Level:
@@ -6,5 +8,13 @@ class Level:
         self.snakes = []
         self.apples = []
         self.blocks = []
+
+        self.snakes.append(Snake((5, 0)))
+        self.snakes[0].direction = 3
+        self.snakes.append(Snake((-5, 1)))
+        self.snakes[1].direction = 1
+
+
+        self.blocks.append(Block((-5, -5)))
 
         self.dimensions = dimensions
