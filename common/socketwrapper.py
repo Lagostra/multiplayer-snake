a384b09cdcfd9f9155c69cdf4bd957190f0397b5
Eivind Lie Andreassen <ela4991@gmail.com>
2017-10-30 13:50:55 +0100

    Added handling of BrokenPipeError to socketwrapper

--- common/socketwrapper.py
+++ common/socketwrapper.py
@@ -49,5 +49,5 @@ class SocketWrapper:
         try:
             self.socket.send(struct.pack('i', len(message)) + message)
             return True
-        except (ConnectionResetError, ConnectionAbortedError):
+        except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError):
             return False
